c5015aa424f72a168be20b8465cca65b002a5161
Гавриш Олександр <sashawwe21@gmail.com>
2024-12-03 15:47:47 +0200

    Update sitemapController.js

--- sitemapController.js
+++ sitemapController.js
@@ -3,15 +3,28 @@ const { SitemapStream, streamToPromise } = require('sitemap');
 const Product = require('./models/Product');
 const Category = require('./models/Category');
 
-// Функція для форматування імені (замінює пробіли, слеші тощо на дефіси)
-const formatName = (name) => {
-  if (!name) return 'новий-товар'; // Якщо назва порожня
-  return name
+const transliterate = (text) => {
+  const cyrillicToLatinMap = {
+    а: 'a', б: 'b', в: 'v', г: 'g', д: 'd', е: 'e', є: 'ye', ж: 'zh',
+    з: 'z', и: 'y', і: 'i', ї: 'yi', й: 'y', к: 'k', л: 'l', м: 'm',
+    н: 'n', о: 'o', п: 'p', р: 'r', с: 's', т: 't', у: 'u', ф: 'f',
+    х: 'kh', ц: 'ts', ч: 'ch', ш: 'sh', щ: 'shch', ю: 'yu', я: 'ya',
+    А: 'A', Б: 'B', В: 'V', Г: 'G', Д: 'D', Е: 'E', Є: 'Ye', Ж: 'Zh',
+    З: 'Z', И: 'Y', І: 'I', Ї: 'Yi', Й: 'Y', К: 'K', Л: 'L', М: 'M',
+    Н: 'N', О: 'O', П: 'P', Р: 'R', С: 'S', Т: 'T', У: 'U', Ф: 'F',
+    Х: 'Kh', Ц: 'Ts', Ч: 'Ch', Ш: 'Sh', Щ: 'Shch', Ю: 'Yu', Я: 'Ya',
+    "'": '', "’": '', "ʼ": '',
+  };
+
+  return text
+    .split('')
+    .map(char => cyrillicToLatinMap[char] || char) // Заміна кирилиці на латиницю
+    .join('')
     .toLowerCase()
     .trim()
     .replace(/[/\\]/g, '-') // Заміна "/" і "\" на дефіс
     .replace(/\s+/g, '-') // Заміна пробілів на дефіс
-    .replace(/[^a-z0-9а-яёїієґ-]/gi, ''); // Видалення небажаних символів
+    .replace(/[^a-z0-9-]/g, ''); // Видалення інших небажаних символів
 };
 
 const generateSitemap = async (req, res) => {
@@ -30,7 +43,7 @@ const generateSitemap = async (req, res) => {
     const products = await Product.find();
     products.forEach(product => {
       sitemap.write({
-        url: `/catalog/${product._id}/${formatName(product.name)}`,
+        url: `/catalog/${product._id}/${transliterate(product.name)}`,
         changefreq: 'weekly',
         priority: 0.7,
       });
@@ -40,7 +53,7 @@ const generateSitemap = async (req, res) => {
     const categories = await Category.find();
     categories.forEach(category => {
       sitemap.write({
-        url: `/category/${category._id}/${formatName(category.label)}`,
+        url: `/category/${category._id}/${transliterate(category.label)}`,
         changefreq: 'weekly',
         priority: 0.7,
       });
